35ec5dd1acb3ba5d260728ca37c1919af6a10c62
Rakhi <rakhigopchandani84@gmail.com>
2022-02-20 16:51:33 +0500

    day 13 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,15 +1,32 @@
-
-
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, avoid_print, unused_import
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:my_app/models/catalog.dart';
-
+import 'dart:convert';
 import '../utils/widgets/drawer.dart';
 import '../utils/widgets/item_widget.dart';
 
 // ignore: use_key_in_widget_constructors
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var  productsData=decodedData["products"];
+    print(productsData);
+  }
+
   @override
   Widget build(BuildContext context) {
     //int days = 30;
@@ -18,28 +35,20 @@ class HomePage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        
-      
-        title: Center(child: Text("CATALOG APP"
-        )),
+        title: Center(child: Text("CATALOG APP")),
       ),
-      body:Padding(
+      body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: ListView.builder(
-         itemCount: dummyList.length,
-         itemBuilder: (context, index){
-          return ItemWidget(item: dummyList[index],
-          
-          );
-
-         },
-
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
-      ), 
-    
-  
+      ),
       drawer: MyDrawer(),
-  
     );
   }
 }
